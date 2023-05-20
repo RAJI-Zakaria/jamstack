f45303e389ed1b147697b8d8deec39d4d971c63d
RAJI-Zakaria <zakariaraji.me@gmail.com>
2023-05-21 01:35:42 +0200

    Refactoring Photos page and adding search feature

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -2,53 +2,68 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import styles from '../styles/Home.module.css'
 
-//import config file
-const { PHOTOS_API } = require('../config/default.json')
+// Import config file
+import { PHOTOS_API } from '../config/default.json'
 
 const Photos: React.FC = () => {
   const [photos, setPhotos] = useState([])
+  const [searchQuery, setSearchQuery] = useState('batman')
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    const fetchPhotos = async () => {
-      console.warn('PHOTOS_API:', PHOTOS_API)
-      try {
-        //photos?query=batman
-        const response = await axios.get(PHOTOS_API.URL, {
-          params: {
-            client_id: PHOTOS_API.ACCESS_KEY,
-            query: 'batman',
-          },
-        })
-
-        setPhotos(response.data.results)
-      } catch (error) {
-        console.error('Error fetching photos:', error)
-      }
-    }
-
     fetchPhotos()
   }, [])
 
-  useEffect(() => {
-    console.log('photos:', photos)
-  }, [photos])
+  const fetchPhotos = async () => {
+    try {
+      setLoading(true)
+
+      const response = await axios.get(PHOTOS_API.URL, {
+        params: {
+          client_id: PHOTOS_API.ACCESS_KEY,
+          query: searchQuery,
+        },
+      })
+
+      setPhotos(response.data.results)
+      setLoading(false)
+    } catch (error) {
+      console.error('Error fetching photos:', error)
+      setLoading(false)
+    }
+  }
+
+  const handleSearch = () => {
+    fetchPhotos()
+  }
 
   return (
     <div className={styles.main}>
       <h1>Unsplash and Axios are amazing!</h1>
-      <ul>
-        {photos.map((photo) => (
-          <li key={photo.id}>
-            {/* add style 100% width  to img */}
-
-            <img
-              className={styles.cll}
-              src={photo.urls.full}
-              alt={photo.alt_description}
-            />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {photos.length > 0 &&
+            photos.map((photo) => (
+              <li key={photo.id}>
+                <img
+                  className={styles.cll}
+                  src={photo.urls.full}
+                  alt={photo.alt_description}
+                />
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   )
 }
