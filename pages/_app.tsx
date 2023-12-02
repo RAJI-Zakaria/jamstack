dc54550831a040ec30219368e6f2b12e91beb54c
zak <zak@zaks-MacBook-Pro.local>
2023-12-02 23:42:57 +0100

    modifying unsplash system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import {AppProps} from "next/app"
 import '../styles/globals.css';
+import { SSRProvider } from '@react-aria/ssr';
 
 export default function App({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
+  return (
+  
+  <SSRProvider>
+    <Component {...pageProps} />
+  </SSRProvider>
+  )
 }
 
--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,71 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import styles from '../styles/Home.module.css'
-import { Radio } from '@nextui-org/react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Radio, Image } from '@nextui-org/react';
+import ENV from '../config/default.json';
+import LoadingScreen from '../components/loading/LoadingScreen';
 
-// Import config file
-import { PHOTOS_API } from '../config/default.json'
-
-//importing loading screen
-
-import LoadingScreen from '../components/loading/LoadingScreen'
+const { PHOTOS_API } = ENV;
 
 const Photos: React.FC = () => {
-  const [photos, setPhotos] = useState([])
-  const [searchQuery, setSearchQuery] = useState('batman')
-  const [loading, setLoading] = useState(false)
-  const [page, setPage] = useState(1)
-  const [q, setQ] = useState('thumb')
+  const [photos, setPhotos] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('batman');
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [q, setQ] = useState('thumb');
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
 
   useEffect(() => {
-    fetchPhotos()
-  }, [])
-  useEffect(() => {
-    fetchPhotos()
-  }, [page])
+    fetchPhotos();
+  }, [page]);
 
   const fetchPhotos = async () => {
     try {
-      setLoading(true)
-
-      // @ts-ignore
+      setLoading(true);
       const response = await axios.get(PHOTOS_API.URL, {
         params: {
           client_id: PHOTOS_API.ACCESS_KEY,
           query: searchQuery,
           page: page,
         },
-      })
+      });
 
       if (page === 1) {
-        setPhotos(response.data.results)
+        setPhotos(response.data.results);
       } else {
-
-        // @ts-ignore
-        setPhotos((prevPhotos) => [...prevPhotos, ...response.data.results])
+        setPhotos((prevPhotos) => [...prevPhotos, ...response.data.results]);
       }
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
-      console.error('Error fetching photos:', error)
-      setLoading(false)
+      console.error('Error fetching photos:', error);
+      setLoading(false);
     }
-  }
+  };
 
   const handleSearch = () => {
-    setPage(1)
-    fetchPhotos()
-  }
+    setPage(1);
+    fetchPhotos();
+  };
 
   const handleLoadMore = () => {
-    setPage((prevPage) => prevPage + 1)
-  }
+    setPage((prevPage) => prevPage + 1);
+  };
 
-  // @ts-ignore
-  const handleKeyPress = (e) => {
+  const handleKeyPress = (e: any) => {
     if (e.key === 'Enter') {
-      handleSearch()
+      handleSearch();
     }
-  }
+  };
+
+  const handleImageClick = (photo: any) => {
+    window.open(photo.urls[q], '_blank');
+  };
 
   const options = [
     { id: '1', title: 'raw' },
@@ -73,66 +66,87 @@ const Photos: React.FC = () => {
     { id: '3', title: 'regular' },
     { id: '4', title: 'small' },
     { id: '5', title: 'thumb' },
-  ]
+  ];
+
   const renderOption = () => {
     return (
       <Radio.Group value={q} onChange={setQ} aria-label="Options">
-        {options.map((option, index) => (
-          <Radio labelColor="success" key={index} value={option.title}>
-            {option.title}
-          </Radio>
-        ))}
+        <div className="flex justify-center">
+          {options.map((option, index) => (
+            <Radio  key={index} labelColor="success" value={option.title}>
+              {option.title}
+            </Radio>
+          ))}
+        </div>
       </Radio.Group>
-    )
-  }
-
-  // @ts-ignore
+    );
+  };
+  
+  
   return (
-    <div className={styles.main}>
-      <h1>Unsplash and Axios are amazing!</h1>
-      <div>
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-
-        <Radio.Group label="Options" defaultValue="A">
+    <div className="flex justify-center items-center">
+    <div className="p-4 ">
+        <h1 className="text-4xl mb-4 text-center">Unsplash and Axios are amazing!</h1>
+        <div className="max-w-xl w-full m-auto justify-center  mb-4">
+          <input
+            className="p-2 mb-2 border border-gray-300 rounded w-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 w-full"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
           {renderOption()}
-        </Radio.Group>
+        </div>
 
-        <button onClick={handleSearch}>Search</button>
+        <div className="flex flex-wrap justify-center gap-4">
+          {photos &&
+            photos.map((photo: any, index) => (
+              <div key={photo.id} className="w-1/6">
+                <Image
+                  width={300}
+                  height={200}
+                  className="w-full h-auto rounded"
+                  src={photo.urls[q]}
+                  alt={photo.alt_description}
+                  onClick={() => handleImageClick(photo)}
+                  objectFit="cover"
+                />
+              </div>
+            ))}
+        </div>
+
+        {selectedPhoto && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-10">
+            <div className="max-w-2xl max-h-2xl overflow-auto">
+              <Image
+                className="w-full h-auto"
+                src={selectedPhoto.urls.full}
+                alt={selectedPhoto.alt_description}
+              />
+            </div>
+          </div>
+        )}
+
+        {photos && photos.length > 0 && !loading && (
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded mt-4 hover:bg-blue-600 w-full"
+            onClick={handleLoadMore}
+          >
+            Load More
+          </button>
+        )}
+
+        {loading && <LoadingScreen />}
       </div>
-
-      <ul>
-        {photos.map((photo, index) => (
-
-            // @ts-ignore
-          <li key={photo.id}>
-
-            <img
-              className={styles.cll}
-              src={
-                // @ts-ignore
-                photo.urls[q]
-              }
-              alt={
-                // @ts-ignore
-                photo.alt_description
-              }
-            />
-          </li>
-        ))}
-      </ul>
-
-      {photos.length > 0 && !loading && (
-        <button onClick={handleLoadMore}>Load More</button>
-      )}
-
-      {loading && <LoadingScreen />}
     </div>
-  )
-}
+  );
+};
 
-export default Photos
+
+export default Photos;
